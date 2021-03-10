b77f3ab3173886ce32c5b4bb6b494fb5d7433738
Verron Knowles <Verron.Knowles@develme.com>
2021-03-09 23:17:27 -0500

    New order capability for model engine ordering.

--- src/Engines/Model.php
+++ src/Engines/Model.php
@@ -5,9 +5,13 @@ namespace DevelMe\RestfulList\Engines;
 
 use Closure;
 use DevelMe\RestfulList\Contracts\Comparator\Composer;
+use DevelMe\RestfulList\Contracts\Orders\Arrangement;
 use DevelMe\RestfulList\Contracts\Engine\Data;
-use DevelMe\RestfulList\Contracts\Filters\Setting as SettingInterface;
-use DevelMe\RestfulList\Filters\Setting;
+use DevelMe\RestfulList\Contracts\Filters\Setting as FilterSettingInterface;
+use DevelMe\RestfulList\Contracts\Orders\Setting as OrderSettingInterface;
+use DevelMe\RestfulList\Filters\Setting as FilterSetting;
+use DevelMe\RestfulList\Orders\Setting as OrderSetting;
+use Exception;
 use Illuminate\Database\Eloquent\Builder;
 
 class Model implements Data
@@ -15,6 +19,8 @@ class Model implements Data
     private Builder $data;
 
     private array $filters = [];
+
+    private array $orders = [];
 
     private bool $prepared = false;
 
@@ -26,10 +32,13 @@ class Model implements Data
 
     private Composer $comparator;
 
-    public function __construct(Builder $data, Composer $comparator)
+    private Arrangement $arrangement;
+
+    public function __construct(Builder $data, Composer $comparator, Arrangement $arrangement)
     {
         $this->data = $data;
         $this->comparator = $comparator;
+        $this->arrangement = $arrangement;
     }
 
     public function filters(array $filters): self
@@ -39,17 +48,24 @@ class Model implements Data
         return $this;
     }
 
-    public function sql()
+    public function orders(array $orders): self
+    {
+        $this->orders = $orders;
+
+        return $this;
+    }
+
+    public function sql(): string
     {
         return $this->prepareModel()->toSql();
     }
 
-    public function bindings()
+    public function bindings(): array
     {
         return $this->prepareModel()->getBindings();
     }
 
-    public function count()
+    public function count(): int
     {
         return $this->prepareModel()->count();
     }
@@ -63,6 +79,7 @@ class Model implements Data
     {
         if ($this->prepared === false) {
             $this->applyFilters();
+            $this->applyOrders();
 
             $this->prepared = true;
         }
@@ -70,14 +87,33 @@ class Model implements Data
         return $this->data;
     }
 
+    /**
+     * @throws Exception
+     */
     protected function applyFilters()
     {
-        foreach ($this->filters as $name => $search) {
-            match(gettype($search)) {
-                "string" => $this->data->where($name, '=', $search),
-                "array" => $this->comparator->compare($this->arrayToSetting($name, $search), $this),
-                "object" => $this->comparator->compare($search, $this),
-                default => throw new \Exception("Type not supported: " . gettype($search))
+        foreach ($this->filters as $name => $setting) {
+            match(true) {
+                is_string($setting) => $this->data->where($name, '=', $setting),
+                is_array($setting) => $this->comparator->compare($this->convertArrayToFilterSetting($name, $setting), $this),
+                $setting instanceof FilterSettingInterface => $this->comparator->compare($setting, $this),
+                default => throw new Exception("Type not supported: " . gettype($setting))
+            };
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function applyOrders()
+    {
+        foreach ($this->orders as $name => $setting) {
+            match(true) {
+                is_string($setting) && is_numeric($name) => $this->data->orderBy($setting),
+                is_string($setting) && is_string($name) => $this->data->orderBy($name, $setting),
+                is_array($setting) => $this->arrangement->arrange($this->convertArrayToOrderSetting($name, $setting), $this),
+                $setting instanceof OrderSettingInterface => $this->arrangement->compare($setting, $this),
+                default => throw new Exception("Type not supported: " . gettype($setting))
             };
         }
     }
@@ -87,12 +123,20 @@ class Model implements Data
         return $this->data;
     }
 
-    private function arrayToSetting($name, array $search): SettingInterface
+    private function convertArrayToFilterSetting($name, array $setting): FilterSettingInterface
     {
-        return new Setting(
-            field: $search['field'] ?? $name,
-            type: $search['type'] ?? 'equals',
-            value: $search['value'] ?? $search
+        return new FilterSetting(
+            field: $setting['field'] ?? $name,
+            type: $setting['type'] ?? 'equals',
+            value: $setting['value'] ?? $setting
+        );
+    }
+
+    private function convertArrayToOrderSetting($name, array $setting): OrderSettingInterface
+    {
+        return new OrderSetting(
+            field: $setting['field'] ?? $name,
+            direction: $setting['direction'] ?? 'asc',
         );
     }
 }

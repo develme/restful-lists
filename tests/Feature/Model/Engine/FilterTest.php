6bbd5468b265cbbc322ff0229b2d727ad8c3cfab
Verron Knowles <Verron.Knowles@develme.com>
2021-03-12 03:29:53 -0500

    New migration for testing engine against database. New tests to verify engine filters database records properly.

---
+++ tests/Feature/Model/Engine/FilterTest.php
@@ -0,0 +1,254 @@
+<?php
+
+
+namespace Tests\Feature\Model\Engine;
+
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use ReflectionException;
+use Tests\Feature\Model\TestCase;
+use Tests\Models\Example;
+use Closure;
+
+/**
+ * Class FilterTest
+ * @package Tests\Feature\Model\Engine
+ * @group Filter
+ * @group Model
+ * @group Engine
+ */
+class FilterTest extends TestCase
+{
+    /**
+     * @test
+     * @throws Exception
+     */
+    public function it_has_simplified_filtering()
+    {
+        $filters = ['type' => 'Open'];
+
+        Example::factory($this->faker->numberBetween(10, 15))->closed()->create();
+        Example::factory($this->faker->numberBetween(10, 15))->open()->create();
+
+        $example = Example::where('type', 'Open')->get();
+        $engine = $this->generateEngine(['data' => Example::query()]);
+
+        $this->assertEquals($example->count(), $engine->filters($filters)->count());
+    }
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function it_has_complex_filtering()
+    {
+        $filters = [
+            'type' => [
+                'field' => 'type',
+                'type' => 'not_equals',
+                'value' => 'Closed'
+            ],
+            'email' => [
+                'field' => 'email',
+                'type' => 'contains',
+                'value' => 'example',
+            ]
+        ];
+
+        Example::factory($this->faker->numberBetween(10, 15))->closed()->create();
+        Example::factory($this->faker->numberBetween(10, 15))->open()->create();
+
+        $example = Example::where('type', '!=', 'Closed')->where('email', 'like', '%example%')->get();
+        $engine = $this->generateEngine(['data' => Example::query()]);
+
+        $this->assertEquals($example->count(), $engine->filters($filters)->count());
+    }
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function it_filters_equals()
+    {
+        $search = $this->faker->word;
+
+        $this->checkFilterTypeAgainstResource(
+            type: "equals",
+            resourceHandle: fn(Builder $query): Collection => $query->where('type', '=', $search)->get(),
+            value: $search
+        );
+    }
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function it_filters_not_equals()
+    {
+        $search = $this->faker->word;
+
+        $this->checkFilterTypeAgainstResource(
+            type: "not_equals",
+            resourceHandle: fn(Builder $query): Collection => $query->where('type', '!=', $search)->get(),
+            value: $search
+        );
+    }
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function it_filters_contains()
+    {
+        $search = $this->faker->word;
+        $value = $this->faker->word . " " . $search . " " . $this->faker->word;
+
+        $this->checkFilterTypeAgainstResource(
+            type: "contains",
+            resourceHandle: fn(Builder $query): Collection => $query->where('type', 'like', "%$search%")->get(),
+            value: $value,
+            search: $search
+        );
+    }
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function it_filters_starts_with()
+    {
+        $search = $this->faker->word;
+        $value = $search . " " . $this->faker->word;
+
+        $this->checkFilterTypeAgainstResource(
+            type: "starts_with",
+            resourceHandle: fn(Builder $query): Collection => $query->where('type', 'like', "$search%")->get(),
+            value: $value,
+            search: $search
+        );
+    }
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function it_filters_ends_with()
+    {
+        $search = $this->faker->word;
+        $value = $this->faker->word . " " . $search;
+
+        $this->checkFilterTypeAgainstResource(
+            type: "ends_with",
+            resourceHandle: fn(Builder $query): Collection => $query->where('type', 'like', "%$search")->get(),
+            value: $value,
+            search: $search
+        );
+    }
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function it_filters_in()
+    {
+        $value = $this->faker->word;
+        $search = [$value];
+
+        $this->checkFilterTypeAgainstResource(
+            type: "in",
+            resourceHandle: fn(Builder $query): Collection => $query->whereIn('type', $search)->get(),
+            value: $value,
+            search: $search
+        );
+    }
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function it_filters_between()
+    {
+        $date = $this->faker->dateTime;
+        $start = Carbon::parse($date);
+        $search['from'] = $start->format('Y-m-d');
+        $value = $start->addDays($this->faker->numberBetween(5, 10))->format('Y-m-d');
+        $search['to'] = Carbon::parse($date)->addDays($this->faker->numberBetween(15, 20))->format('Y-m-d');
+
+        $this->checkFilterTypeAgainstResource(
+            type: "between",
+            resourceHandle: fn(Builder $query): Collection => $query->where('type', '>=', $search['from'])->where('type', '<=', $search['to'])->get(),
+            value: $value,
+            search: $search
+        );
+    }
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function it_filters_less_than()
+    {
+        $search = $this->faker->numberBetween(11, 15);
+        $value = $this->faker->numberBetween(5, 10);
+
+        $this->checkFilterTypeAgainstResource(
+            type: "less_than",
+            resourceHandle: fn(Builder $query): Collection => $query->where('type', '<', $search)->get(),
+            value: $value,
+            search: $search
+        );
+    }
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function it_filters_less_than_or_equal()
+    {
+        $search = $this->faker->numberBetween(8, 15);
+        $value = $this->faker->numberBetween(5, 12);
+
+        $this->checkFilterTypeAgainstResource(
+            type: "less_than_or_equal",
+            resourceHandle: fn(Builder $query): Collection => $query->where('type', '<=', $search)->get(),
+            value: $value,
+            search: $search
+        );
+    }
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function it_filters_greater_than()
+    {
+        $search = $this->faker->numberBetween(5, 10);
+        $value = $this->faker->numberBetween(11, 15);
+
+        $this->checkFilterTypeAgainstResource(
+            type: "greater_than",
+            resourceHandle: fn(Builder $query): Collection => $query->where('type', '>', $search)->get(),
+            value: $value,
+            search: $search
+        );
+    }
+
+    /**
+     * @test
+     * @throws ReflectionException
+     */
+    public function it_filters_greater_tha_or_equal()
+    {
+        $search = $this->faker->numberBetween(5, 102);
+        $value = $this->faker->numberBetween(8, 15);
+
+        $this->checkFilterTypeAgainstResource(
+            type: "greater_than_or_equal",
+            resourceHandle: fn(Builder $query): Collection => $query->where('type', '>=', $search)->get(),
+            value: $value,
+            search: $search
+        );
+    }
+}
---
+++ tests/Feature/Model/TestCase.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Tests\Feature\Model;
+
+use DevelMe\RestfulList\Engines\Model as ModelEngine;
+use DevelMe\RestfulList\Orchestration\Model\Orchestrator;
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Database\Schema\Blueprint;
+use Tests\Traits\WithEloquent;
+use Tests\Traits\WithEngineParts;
+use Tests\Traits\WithFaker;
+
+class TestCase extends \Tests\TestCase
+{
+    use WithEngineParts, WithFaker, WithEloquent;
+
+    protected array $engine = [
+        'engine' => ModelEngine::class,
+        'orchestrator' => Orchestrator::class,
+    ];
+
+    protected function migrateDatabase(Manager $schema): void
+    {
+        $schema->schema()->create('examples', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->dateTime('email_verified_at')->nullable();
+            $table->string('description')->nullable();
+            $table->string('password', 500)->nullable();
+            $table->string('remember_token')->nullable();
+            $table->string('type')->nullable();
+
+            $table->timestamps();
+        });
+    }
+}

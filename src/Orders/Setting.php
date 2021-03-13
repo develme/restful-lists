f4d31cce837896a0160834523b26938dd8e7a099
Verron Knowles <Verron.Knowles@develme.com>
2021-03-13 16:07:19 -0500

    Order setting modifications to handle string and array setting passes.

--- src/Orders/Setting.php
+++ src/Orders/Setting.php
@@ -10,10 +10,25 @@ class Setting implements \DevelMe\RestfulList\Contracts\Orders\Setting
 
     private string $direction;
 
+    protected static string $defaultDirection = 'asc';
+
     public function __construct(string $field, string $direction)
     {
         $this->field = $field;
         $this->direction = $direction;
+    }
+
+    public static function createFromString(string $field, ?string $direction = null): static
+    {
+        return new static($field, $direction ?? static::$defaultDirection);
+    }
+
+    public static function createFromArray(array $setting, $name): static
+    {
+        return new static(
+            field: $setting['field'] ?? $name,
+            direction: $setting['direction'] ?? static::$defaultDirection,
+        );
     }
 
     public function field(): string

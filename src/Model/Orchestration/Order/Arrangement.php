62890d6e9ec8fa2d5519f4a045327cb6855f7312
Verron Knowles <Verron.Knowles@develme.com>
2021-03-14 18:27:29 -0400

    Wording

--- src/Model/Orchestration/Order/Arrangement.php
+++ src/Model/Orchestration/Order/Arrangement.php
@@ -4,12 +4,11 @@
 namespace DevelMe\RestfulList\Model\Orchestration\Order;
 
 use DevelMe\RestfulList\Contracts\Engine\Data;
-use DevelMe\RestfulList\Contracts\Orders\Arrangement as ArrangementContract;
+use DevelMe\RestfulList\Contracts\Orders\Arrangement as ArrangementInterface;
 use DevelMe\RestfulList\Contracts\Orders\Setting;
 
-class Arrangement implements ArrangementContract
+class Arrangement implements ArrangementInterface
 {
-
     public function arrange(Setting $setting, Data $data)
     {
         $data->data()->orderBy($setting->field(), $setting->direction());

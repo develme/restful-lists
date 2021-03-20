96e3e7fffa058e7b3a955e882ba8fd2891462763
Verron Knowles <Verron.Knowles@develme.com>
2021-03-20 18:15:49 -0400

    Add meta data for pagination. Move count/total to meta area.

--- src/Model/Service/Resource.php
+++ src/Model/Service/Resource.php
@@ -20,10 +20,52 @@ class Resource extends ResourceCollection
 
     public function toArray($request)
     {
-        return [
-            'data' => $this->collection,
-            'count' => $this->engine->count(),
-            'total' => $this->engine->total(),
+
+        $result = [
+            "data" => $this->collection,
+            "meta" => [
+                "count" => $this->engine->count(),
+                "total" => $this->engine->total(),
+            ]
         ];
+
+        if ($request->has('pagination')) {
+            $result['meta']['pagination'] = $this->paginationMeta($request);
+        }
+
+        return $result;
+    }
+
+    private function paginationMeta(\Illuminate\Http\Request $request): array
+    {
+        $params = $request->all();
+        $pagination = data_get($params, 'pagination', []);
+        $page = (int) data_get($pagination, 'page', 1);
+        $size = (int) data_get($pagination, 'size');
+        $total = $this->engine->total();
+        $pages = ceil($total / $size);
+
+        $meta = [
+            'previous' => $page > 1 ? $page - 1 : null,
+            'current' => $page,
+            'next' => $page < $pages ? $page + 1 : null,
+            'size' => $size,
+            'total' => $pages,
+            'urls' => [
+                'previous' => null,
+                'current' => $request->fullUrl(),
+                'next' => null,
+            ],
+        ];
+
+        if ($page < $pages) {
+            $meta['urls']['next'] = $request->url() . '?' . http_build_query(array_merge($params, ['pagination' => array_merge($pagination, ['page' => $page + 1])]), "", "&");
+        }
+
+        if ($page > 1) {
+            $meta['urls']['previous'] = $request->url() . '?' . http_build_query(array_merge($params, ['pagination' => array_merge($pagination, ['page' => $page - 1])]), "", "&");
+        }
+
+        return $meta;
     }
 }
